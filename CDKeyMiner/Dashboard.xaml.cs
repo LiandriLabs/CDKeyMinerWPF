641a91cbfb48f9182097d7ef2db63166be81d9b6
Vladislav Zorov <psionski@gmail.com>
2021-03-30 03:28:07 +0300

    Animation.

--- CDKeyMiner/Dashboard.xaml.cs
+++ CDKeyMiner/Dashboard.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,6 +22,7 @@ namespace CDKeyMiner
     public partial class Dashboard : Page
     {
         private Credentials creds;
+        private bool mining = false;
 
         public Dashboard(Credentials credentials)
         {
@@ -30,7 +32,34 @@ namespace CDKeyMiner
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            greetingLbl.Content = $"Welcome, {creds.Username}";
+            var sb = (Storyboard)FindResource("FadeIn");
+            sb.Begin(this);
+        }
+
+        private void Label_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!mining)
+            {
+                mining = true;
+                buttonLbl.Content = "■";
+                statusLbl.Content = "Mining...";
+            }
+            else
+            {
+                mining = false;
+                buttonLbl.Content = "▶";
+                statusLbl.Content = "Press to start mining.";
+            }
+        }
+
+        private void buttonLbl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Label).Foreground = (Brush)FindResource("MinerHighlight");
+        }
+
+        private void buttonLbl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Label).Foreground = (Brush)FindResource("MinerGreen");
         }
     }
 }
