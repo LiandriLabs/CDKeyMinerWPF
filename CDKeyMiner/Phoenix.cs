0d06434eece322ed2388b8d58cbd53a0a5d04140
nomoredice <95712247+nomoredice@users.noreply.github.com>
2021-12-08 21:46:28 +0200

    Changing mining ports to SSL. (#1)
    Changing mining ports to SSL

--- CDKeyMiner/Phoenix.cs
+++ CDKeyMiner/Phoenix.cs
@@ -121,11 +121,11 @@ namespace CDKeyMiner
             var algo = (Application.Current as App).Algo;
             if (algo == Algo.ETH)
             {
-                phoenixProc.StartInfo.Arguments = $"-pool pool.cdkeyminer.com:9000 -wal {credentials.Username} -proto 2 -coin eth -stales 0 -rate 0 -cdm 0 -gsi 0 -log 1 -logdir Logs";
+                phoenixProc.StartInfo.Arguments = $"-pool ssl://app.cdkeyminer.com:10000 -wal {credentials.Username} -proto 2 -coin eth -stales 0 -rate 0 -cdm 0 -gsi 0 -log 1 -logdir Logs";
             }
             else if (algo == Algo.ETC)
             {
-                phoenixProc.StartInfo.Arguments = $"-pool pool.cdkeyminer.com:9001 -wal {credentials.Username} -proto 2 -coin etc -stales 0 -rate 0 -cdm 0 -gsi 0 -log 1 -logdir Logs";
+                phoenixProc.StartInfo.Arguments = $"-pool ssl://app.cdkeyminer.com:10001 -wal {credentials.Username} -proto 2 -coin etc -stales 0 -rate 0 -cdm 0 -gsi 0 -log 1 -logdir Logs";
             }
             else
             {
